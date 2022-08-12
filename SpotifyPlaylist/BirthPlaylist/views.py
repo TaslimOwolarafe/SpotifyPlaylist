5a5bc1f7d012e6a36b7b8eec47356c71477d6bdd
TaslimOwolarafe <owolarafetaslim@gmail.com>
2022-08-12 10:33:02 +0100

    user profile and refresh

--- SpotifyPlaylist/BirthPlaylist/views.py
+++ SpotifyPlaylist/BirthPlaylist/views.py
@@ -1,3 +1,5 @@
+from email import header
+from glob import glob
 from urllib import response
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
@@ -30,11 +32,12 @@ redirect_uri = os.getenv("redirect_uri")
 
 auth_url = "https://accounts.spotify.com/authorize"
 token_url = "https://accounts.spotify.com/api/token"
+
 track_url = 'https://api.spotify.com/v1/tracks'
 user_url = "https://api.spotify.com/v1/me/"
 user_playlists_url = "https://api.spotify.com/v1/me/playlists"
 
-
+base64_client_cred = ''
 
 def index(request):
     return render(request, 'BirthPlaylist/index.html')
@@ -64,9 +67,11 @@ def home(request):
     return HttpResponseRedirect(reverse("callback", args=[state]))
 
 access_token = ''
+refresh_token = ''
 # def callback(request, code, state):
 def callback(request, state):
     client_creds = f'{client_id}:{client_secret}'
+    global base64_client_cred
     base64_client_cred = base64.b64encode(client_creds.encode()).decode()
     print(code, "\n")
     print(state, "\n")
@@ -87,6 +92,8 @@ def callback(request, state):
     # print(response, "\n")
     global access_token
     access_token = response['access_token']
+    global refresh_token
+    refresh_token = response['refresh_token']
     # token_type = response['token_type']
     # access_token = response['access_token']
     # expires_in = response['expires_in']
@@ -98,23 +105,68 @@ def callback(request, state):
     data = JsonResponse(response)
     return render(request, 'BirthPlaylist/callback.html')
 
+# username = ''
 
-
-def me(request):
-    headers= {
-        'Authorization':f'Bearer {access_token}'
-    }
-    me_response = requests.get(user_url, headers=headers)
-    response = me_response.json()
-    print(response)
-    # username = response['display_name']
-    # print(username)
-    return JsonResponse(response)
+# def me(request):
+#     headers= {
+#         'Authorization':f'Bearer {access_token}'
+#     }
+#     me_response = requests.get(user_url, headers=headers)
+#     response = me_response.json()
+#     print(response)
+#     global username
+#     username = response['display_name']
+#     print(username)
+#     return JsonResponse(response)
 
 def me_playlist(request):
     headers = {
         'Authorization':f'Bearer {access_token}'
     }
-    playlist_response = requests.get(user_playlists_url, headers=headers)
-    response = playlist_response.json()
-    return JsonResponse(response)
+
+    me_response = requests.get(user_url, headers=headers)
+    response_me = me_response.json()
+    print(response_me)
+    print(me_response.status_code)
+    if response_me.get('error') or me_response.status_code != 200:
+
+        ## refresh token and redirect.. tell user token expired
+        return JsonResponse(response_me)
+    else:
+        
+        display_name = response_me['display_name']
+        id = response_me['id']
+        print(display_name, "\t", id)
+        if len(response_me['images']) == 0:
+            image_url = "{% static 'images/skateboard.jpg' %}"
+        else:
+            image_url = response_me['images'][0]['url']
+        # print(response_me['images'][0]['url'])
+        # print(len(response_me['images']))
+        playlist_response = requests.get(user_playlists_url, headers=headers)
+        response = playlist_response.json()
+        # playlists = JsonResponse(response)
+        playlists = response
+        return render(request, "BirthPlaylist/playlists.html", context={'playlists':playlists, 'display_name':display_name, 'id':id,
+            'image_url':image_url})
+
+def refresh(request):
+    refresh_headers = {
+        'Authorization': f'Basic {base64_client_cred}',
+        'Content-Type': "application/x-www-form-urlencoded"
+    }
+    refresh_data = {
+        'grant_type': 'refresh_token',
+        'refresh_token': f'{refresh_token}'
+    }
+
+    token_request = requests.post(token_url, data=refresh_data, headers=refresh_headers)
+    response = token_request.json()
+    print(response)
+    global access_token
+    access_token = response['access_token']
+    return HttpResponseRedirect(reverse(request.path, args=[state]))
+
+def search(request):
+
+    return
